59a03f5e6dc318cd2aab953d2a00ffb9b0838a86
Chepkelembut <103375224+Chepkelembut@users.noreply.github.com>
2022-05-03 14:57:44 +0300

    Add files via upload
    Array

---
+++ C program of an array in reverse order .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+  int ar[9]={34,56,54,32,67,89,90,32,24};
+  
+  printf("write in reverse order as stated above");
+  for(int j=9 ;j>1;j--)
+{
+    printf("%d\n",ar[j]);
+   } 
+    
+    return 0;
+}
